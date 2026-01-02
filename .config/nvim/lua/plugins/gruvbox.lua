0de9d01246fdab5dfbfce90ccac7092ef8290194
Kevin Abrahamsson <kevin.abrahamsson@icomera.com>
2026-01-02 13:48:41 +0100

    Setup color scheme

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,13 +1,16 @@
 return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = true,
-
-    opts = {
-        terminal_colors = true, -- add neovim terminal colors
-    },
+    url = "ellisonleao/gruvbox.nvim",
+    cond = true,
+    priority = 1000, -- Recommended to be high for colorschemes
 
     config = function()
+        require("gruvbox").setup({
+            contrast = "hard" -- Hard or soft
+        })
+
+        vim.o.background = "dark" -- Dark or light
         vim.cmd([[colorscheme gruvbox]])
-    end
+    end,
+
+    lazy = false
 }
