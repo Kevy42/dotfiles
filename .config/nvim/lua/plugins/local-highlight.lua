cc4e10bac2712b80200ea366a511bddea79b46bd
Kevin Abrahamsson <kevin.abrahamsson@icomera.com>
2026-01-27 14:04:28 +0100

    Add local-hightlight

---
+++ .config/nvim/lua/plugins/local-highlight.lua
@@ -0,0 +1,15 @@
+return {
+    url = "https://github.com/tzachar/local-highlight.nvim",
+    cond = true,
+
+    opts = {
+        insert_mode = true, -- Show highlights in INSERT
+        min_match_len = 1,
+
+        animate = {
+            enabled = false, -- requires snacks.nvim
+        },
+    },
+
+    lazy = false, -- Lazy-load plugin
+}
