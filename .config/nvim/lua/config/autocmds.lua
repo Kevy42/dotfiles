021c2f47a7bb6237b9a4087ab647a3dd9dfc3bf7
Kevin Abrahamsson <kevin.abrahamsson@icomera.com>
2026-04-02 16:54:06 +0200

    Update neovim plugins and VIA configuration

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
     desc = "In order to disable continuing comment on new lines",
 })
 
---  Usually handled setting "highlight = { enable = true, }" in nvim-treesitter, but cannot be done due to the new "main" branch version not yet supporting it.
+--  Usually handled by setting "highlight = { enable = true, }" in nvim-treesitter, but cannot be done due to the new "main" branch version not yet supporting it.
 --  Manually calling "vim.treesitter.start()" as temporary fix
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { "markdown", "copilot-chat" }, -- "copilot-chat" is the filetype returned from ":set filetype?" when inside Copilot window
