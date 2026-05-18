128e3af9c207a6f1291da057e13e32d5fcbccba5
Kevin Abrahamsson <kevin.abrahamsson@icomera.com>
2026-05-18 14:43:30 +0200

    Add documentation and templates to conform and nvim-lspconfig. Change shfmt settings. Uninstalled shfmt and Shellcheck from Mason and installed via package manager

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,13 @@ return {
 
     opts = {
         formatters_by_ft = {
+            -- NOTE:
+            -- Many LSPs will automatically invoke formatters and linters if available, allowing for lsp_format = "faillback" to be used
+            -- while still maintaining the same functionality. It is however, in my opinnion, still better to manually invoke them
+            -- as that'll both incrase the robustness of your config (as the blast radius of your LSP breaking will be limited)
+            -- as well as conceptually separate the duties of the formatter, LSP and linter.
+            -- Formatter and linter related settings should thus not be configured via the LSP
+
             lua = { lsp_format = "fallback" },
             sh = { "shfmt" },
         },
@@ -20,6 +27,29 @@ return {
         format_on_save = {
             timeout_ms = 10000,
         },
+
+        -- log_level = vim.log.levels.DEBUG, -- Enable to view formatter binary command invocations when viewing the log (":ConformInfo") and thus the default "args = {}" used by various formatters
+
+        -- Formatter configuration template
+        -- Available substitutions:
+        -- $FILENAME           - Absolute path
+        -- $DIRNAME            - Absolute path
+        -- $RELATIVE_FILEPATH  - Relative path
+        -- $EXTENSION          - File extension
+        --
+        -- formatters = {
+        --     shfmt = {
+        --         args = { "--stdin-from-filename", "$FILENAME" }, -- Overwite default args
+        --         prepend_args = { "-ci" },                        -- Prepend to default args
+        --         append_args = { "-ci" },                         -- Append to default args
+        --     }
+        -- }
+
+        formatters = {
+            shfmt = {
+                args = { "-filename", "$FILENAME", "--indent", "4", "--case-indent", "--space-redirects" },
+            }
+        }
     },
 
     lazy = false, -- Lazy-load plugin
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,16 +1,42 @@
 return {
     url = "https://github.com/neovim/nvim-lspconfig",
-
-    dependencies = {
-        "mason-org/mason.nvim"
-    },
-
     cond = true,
 
     -- Executed after plugin is loaded
     config = function()
-        -- NOTE: Make sure to go through the default options for the language servers in ":h lspconfig-all" after installation
-        vim.lsp.enable("bashls")
+        -- LSP configuration template (taken from ":h lsp")
+        -- NOTE:
+        -- Many LSPs will automatically invoke formatters and linters if available, allowing for lsp_format = "faillback" to be used
+        -- while still maintaining the same functionality. It is however, in my opinnion, still better to manually invoke them
+        -- as that'll both incrase the robustness of your config (as the blast radius of your LSP breaking will be limited)
+        -- as well as conceptually separate the duties of the formatter, LSP and linter.
+        -- Formatter and linter related settings should thus not be configured via the LSP
+
+        -- The default options for each language servers can be found under the "lspconfig-all" help section (":h lspconfig-all").
+        -- vim.lsp.config("bashls", {
+        --     cmd = { "bashls" },
+        --     filetypes = { "bash", "sh" }, -- Filetypes to trigger on
+        --     root_markers = { ".git" }, -- How the project root is determined
+        --
+        --     -- LSP specific settings (schema).
+        --     -- A brief list of options (which should be reviewed upon installation of a new LSP) can be found in the Mason IU. The full list can be found online
+        --     settings = {
+        --         bashIde = {
+        --             globPattern = "*@(.sh|.inc|.bash|.command)"
+        --         }
+        --     }
+        -- })
+
+        vim.lsp.config("bashls", {
+            settings = {
+                bashIde = {
+                    includeAllWorkspaceSymbols = true -- Helps with autocompletion of symbols defined in other files
+                }
+            }
+        })
+
+        vim.lsp.enable("bashls") -- NOTE: This LSP automatically invokes both shellfmt and shellcheck for formatting and linting
+
         vim.lsp.enable("lua_ls")
     end,
 
